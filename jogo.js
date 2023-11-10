80c958212b00b38914261f04bf783fcc2eaf185d
Fernandinha14 <106314872+Fernandinha14@users.noreply.github.com>
2023-11-10 10:52:21 -0300

    js

--- jogo.js
+++ jogo.js
@@ -3,3 +3,26 @@ let checarturno = true;
 let turno;
 const JOGADOR_X= "X";
 const JOGADOR_O="O";
+//inicializa a variavel jogoAcabou como falso, indicando que o jogo esta em andamento.
+let jogoAcabou= false;
+
+//Adiciona um evento de clique ao documento que é acionado quando qualquer elemento é clicado.
+document.addEventlistener("click", (event)=>{
+    //verifica se o elemento clicado possui a classe "celula" e se o jogo ainda não acabou.
+    if (event.target.matches(".celula") && !jogoAcabou) {
+        //chama a função jogar, passando o ID do elemento clicado como argumento.
+        jogar(event.target.id);
+    }
+});  
+
+function jogar(id) {
+    const celula= document.getElementById(id);
+    if (celula.textContent ==="") {
+        turno= checarturno ? JOGADOR_X : JOGADOR_0;
+        checarturno= !checarturno;
+        celula.textcontent = turno;
+        celula.classList.add(turno);
+        celula.removeEventListener("click", jogar);
+        verificarvencedor(turno);
+    }
+}
